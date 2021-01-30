89df99c8dc1c68f27df8b038c5615ce00c55f96d
FriedPhysicist <59540097+FriedPhysicist@users.noreply.github.com>
2021-01-30 19:48:46 +0300

    GGJ
    movement

---
+++ Assets/Scripts/gandalf.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gandalf : MonoBehaviour
+{
+    Animator anim;
+    Rigidbody rb;
+    AudioSource _as;
+
+    private float input_z;
+    private float input_x;
+    private Vector3 move_dir;
+    public float speed;
+    
+    [SerializeField] public AudioClip[] _ac; 
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb=GetComponent<Rigidbody>();
+        _as=GetComponent<AudioSource>(); 
+    } 
+    
+    void Update()
+    { 
+        animations();
+    }
+
+    void FixedUpdate()
+    { 
+        movement();
+    }
+
+    void animations()
+    { 
+        anim.SetBool("forward",input_z>0);
+    }
+
+    void movement()
+    {
+        input_z = Input.GetAxis("Vertical");
+        input_x = Input.GetAxis("Horizontal");
+        
+        move_dir = transform.right * input_x + transform.forward * input_z;
+        rb.MovePosition(rb.position+move_dir*speed);
+    }
+}
---
+++ Assets/Scripts/main_camera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic; 
+using UnityEngine;
+
+public class main_camera : MonoBehaviour 
+{
+
+    public float mouseSensivity=1f;
+    public Transform playerBody; 
+    float xRotation; 
+
+    void Start()
+    { 
+        playerBody=transform.parent; 
+    } 
+
+    void Update()
+    {   
+        if(playerBody==null)
+            playerBody=transform.parent;
+
+        float mouseX=Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;        
+        float mouseY=Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;        
+
+        xRotation-=mouseY;
+        xRotation= Mathf.Clamp(xRotation, -90f,90f);
+
+        transform.localRotation= Quaternion.Euler(xRotation,0f,0f); 
+        if(playerBody!=null) playerBody.Rotate(Vector3.up*mouseX); 
+    } 
+}
